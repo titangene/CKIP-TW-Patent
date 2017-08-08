b976ff8bf0a68cb7578a1c29234b057f124e315b
titangene <titangene01@gmail.com>
2017-08-08 12:41:26 +0800

    Add CKIP connection config

---
+++ src/ckip/CKIPSource.java
@@ -0,0 +1,39 @@
+package ckip;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import tw.cheyingwu.ckip.CKIP;
+
+class CKIPSource {
+	private CKIP ckip;
+	private Properties props;
+	private String ip;
+	private int port;
+	private String username;
+	private String password;
+	
+	public CKIPSource() throws IOException, ClassNotFoundException {
+		this("ckip.properties");			// 讀取 ckip 連接設定檔
+	}
+	
+	public CKIPSource(String configFile) throws IOException {
+		props = new Properties();
+		props.load(new FileInputStream("properties/" + configFile));
+		
+		ip = props.getProperty("ckipConnection.ip");
+		port = Integer.parseInt(props.getProperty("ckipConnection.port"));
+		username = props.getProperty("ckipConnection.username");
+		password = props.getProperty("ckipConnection.password");
+	}
+	
+	public CKIP getConnection() {
+		ckip = new CKIP(ip, port, username, password);
+		return ckip;
+	}
+	
+	public CKIP getCKIP() {
+		return ckip;
+	}
+}
